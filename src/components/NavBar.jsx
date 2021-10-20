edafffe239fa0474c8f972abf23a1f070ee42fa1
Agustin Barrozo <agusbarrozo@gmail.com>
2021-10-20 20:02:53 -0300

    agregando NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,22 @@
+const NavBar = () => {
+    return(
+        <div>
+            <nav>
+            <div><a>bismoke</a></div>
+            <div><a>about us</a></div>
+            <div><a>products</a></div>
+            <div><a>find a store</a></div>
+            <div><a>learn</a></div>
+            <div><a>get in touch</a></div>
+            <div>
+                <form action="">
+                    <input type="search" placeholder="search" name="buscador">
+                </form>
+            </div>
+            </nav>
+        </div>
+
+
+    )
+}
+export defautl NavBar
