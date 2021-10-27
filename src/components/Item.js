60971db7953a71cf2b6e72c82cf676059752bc85
Agustin Barrozo <agusbarrozo@gmail.com>
2021-10-27 10:29:56 -0300

    Modificando card groups de productos

--- src/components/Item.js
+++ src/components/Item.js
@@ -98,19 +98,19 @@ const Item = (props) => {
             <Card>
               <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
               <CardBody>
-                <CardTitle tag="h5">Card title</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Button</Button>
+                <CardTitle tag="h5"> {prod.name} </CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted"> ${prod.price} </CardSubtitle>
+                <CardText>* Product #2 description *</CardText>
+                <Button><CartWidget /></Button>
               </CardBody>
             </Card>
             <Card>
               <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
               <CardBody>
-                <CardTitle tag="h5">Card title</CardTitle>
-                <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-                <Button>Button</Button>
+                <CardTitle tag="h5"> {prod.name} </CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted"> ${prod.price} </CardSubtitle>
+                <CardText>* Product #3 description *</CardText>
+                <Button><CartWidget /></Button>
               </CardBody>
             </Card>
           </CardGroup>
